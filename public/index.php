6e2e757254e70c87f919fe4b7975c11570d1211a
aR7dx <158834512+aR7dx@users.noreply.github.com>
2025-11-17 18:23:49 +0100

    suppresion d'une requete php inutile

--- public/index.php
+++ public/index.php
@@ -1,8 +1,6 @@
 <?php
 require '../vendor/autoload.php';
 
-use App\Models\Database\DatabaseModel;
-
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
@@ -35,15 +33,6 @@ if (is_array($match)) {
         if (session_status() === PHP_SESSION_NONE) {
             session_start();
         }
-
-        $sql = "SELECT count(*) as total FROM GEO_EQUIPEMENT";
-        $stmt = DatabaseModel::preparerRequetePDO($sql);
-        $donnees = array();
-        DatabaseModel::LireDonneesPDOPreparee($stmt, $donnees);
-
-        echo '<pre>';
-        var_dump($donnees[0]);
-        echo '</pre>';
 
         call_user_func_array([$controllerInstance, $method], $match['params']);
     }
